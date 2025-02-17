96e4e6f8ee55edc6b373d0ebf15167dbb941a591
Rohit Pradhan <85367563+Rohit734@users.noreply.github.com>
2025-02-17 16:51:18 +0000

    conversion

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "33aa"
+
+console.log(typeof score);
+
+// All data type starts with Capital letter Number() String()
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33abc" -> NaN not a number but type is number
+// 1 -> true
+// 0 -> false
+
+
