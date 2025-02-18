d496719728b04301ae0995beeb0b4d678c527888
Rohit Pradhan <85367563+Rohit734@users.noreply.github.com>
2025-02-18 05:34:17 +0000

    stack and heap

---
+++ basics/05_memories.js
@@ -0,0 +1,9 @@
+/*
+
+Two type of memories HEAP (NON-PRIMITIVE) and STACK (PRIMITIVE)
+STACK (PRIMITIVE) -> whenever we declare variable using strack memory we get copy of variable.
+
+HEAP (NON-PRIMITIVE) - > whenever we declare variable using heap memory we get reference of value
+i.e if we make change then it will change the original value.
+
+*/
