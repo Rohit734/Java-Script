9f888a4d9721d7dc987b9016ac5623eab189cb44
Rohit Pradhan <85367563+Rohit734@users.noreply.github.com>
2025-02-18 04:34:37 +0000

    dataType summary

--- basics/02_datatypes.js
+++ basics/02_datatypes.js
@@ -15,3 +15,47 @@ let isLoggedIn = false // boolen
 
 console.log(typeof undefined);
 
+
+/* 
+----------------------summary--------------------------
+
+1 -> Primitive DATATYPE (call by value orginal data is not changed only the copy is changed )
+there are 7 type they are as follows:
+String
+Number
+Bollean
+null
+undefined
+Symbol - used to make some data unique
+BigInt
+
+2 -> Non Primitive (Reference type we can change value directly in the address) 
+there are three type they are as follows.
+Array
+Object
+Function
+
+*/ 
+
+console.log("-------------SYMBOL------------");
+
+let id = Symbol("123")
+let anotherId = Symbol("123")
+console.log(id);
+console.log(anotherId);
+console.log(id === anotherId);
+
+
+console.log("-------------Non Primitive (Reference type)------------");
+
+let names = ['abc','ram','sam'];
+
+let myObject = {a : names,
+    b : "r",
+}
+ // we can store function in a variable also
+
+ const myFun = function(){
+    console.log("HI i am Function");
+    
+ }
