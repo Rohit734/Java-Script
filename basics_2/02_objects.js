43f84a9dfe1062d74d2fac9e2320da6a05e1b504
Rohit734 <officialrohit734@gmail.com>
2025-03-06 11:12:51 +0530

    objcts

--- basics_2/02_objects.js
+++ basics_2/02_objects.js
@@ -1,10 +1,40 @@
 // singleTon
 // const user = new Object()
 
-const user = {
-    id:"123abc",
-    name: "sam",
-    isLoggedIn : true,
+const user = {}
+user.id = 123
+user.name = "Sam"
+user.isLoggedIn = true
+
+// console.log(user);
+
+const user2 = {
+    email: "gpt@gmail.com",
+    fullName: {
+        userFullName: {
+            first: "Sam",
+            last: "Rai"
+        }
+    },
 }
 
-console.log(user);
+// console.log(user2.fullName.userFullName.first)
+// // using ?
+// console.log(user2.fullName?.userFullName.first)
+
+// Combining objects using asssign
+
+const obj = {1:"a",2:"4"}
+const obj1 = {3:"a",4:"4"}
+const obj2 = Object.assign({},obj,obj1)
+console.log(obj2)
+
+// Combining objects using Spread operator (...)
+const obj3 = {...obj,...obj1}
+console.log(obj3)
+// getting keys 
+console.log(Object.keys(user)) 
+// getting Values 
+console.log(Object.values(user)) 
+console.log(user.hasOwnProperty("email"));
+
