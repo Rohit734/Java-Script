96e4fce5721809ba4c98dc06d481f6f64a907fe1
Rohit734 <officialrohit734@gmail.com>
2025-03-06 11:27:02 +0530

    objects

---
+++ basics_2/03_objects.js
@@ -0,0 +1,10 @@
+ const course = {
+    coursename : "python",
+    price : "999",
+    courseInstructuctor : "Rohit"
+ }
+
+//  destructure of an object
+ const {courseInstructuctor : instructor} = course
+ console.log(instructor);
+ 
