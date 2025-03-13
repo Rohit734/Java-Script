dfc7ac5dfc15e60d1b66c2389f5ceef89ab8b0da
Rohit734 <officialrohit734@gmail.com>
2025-03-13 17:45:23 +0530

    SCOPE in Function

--- functions/01_function.js
+++ functions/01_function.js
@@ -33,3 +33,24 @@ function calculateCartPrice(...num){
 }
 console.log(calculateCartPrice(1,2,3,4))
 
+// Scope in function
+
+let a = 100
+const b = 500
+var c = 300
+
+if(true){
+    
+    let a = 10
+    console.log(`inside Scope value of a ${a}`);
+    const b = 20
+    console.log(`inside Scope value of b ${b}`);
+    var c = 30 // does not recreate new variable but instead overwrites the orginal value
+    console.log(`inside Scope value of c ${c}`);
+}
+
+console.log(`outside Scope value of a ${a}`); // global variable
+console.log(`outside Scope value of b ${b}`); // global variable
+console.log(`outside Scope value of c ${c}`); // global vaariable but value got changed inside function scope.
+
+// NOTE: var is a global SCOPE whereas let and const is a block SCOPE
